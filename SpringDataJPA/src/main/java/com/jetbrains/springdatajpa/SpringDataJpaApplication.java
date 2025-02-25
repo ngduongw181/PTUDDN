b23c9210e8dddd9754280ee2fb6853515dfee6df
duongw.n <duongnguyen1812004@gmail.com>
2025-02-25 21:55:51 +0700

    upload

---
+++ SpringDataJPA/src/main/java/com/jetbrains/springdatajpa/SpringDataJpaApplication.java
@@ -0,0 +1,32 @@
+package com.jetbrains.springdatajpa;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class SpringDataJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataJpaApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner run(EmployeeRepository repository) {
+        return (args -> {
+//            insertJavaAdvocates(repository);
+//            System.out.println(repository.findAll());
+
+            System.out.println(repository.findEmployeeByLastNameContaining(" "));
+
+        });
+    }
+
+    private void insertJavaAdvocates(EmployeeRepository repository) {
+        repository.save(new Employee("Dalia", "Abo Sheasha"));
+        repository.save(new Employee("Trisha", "Gee"));
+        repository.save(new Employee("Helen", "Scott"));
+        repository.save(new Employee("Mala", "Gupta"));
+    }
+}
